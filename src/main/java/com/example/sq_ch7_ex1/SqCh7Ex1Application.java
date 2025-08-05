9980d17b1bc0cf95c6644f80fc021fa73f6b539b
maxi-aguero <maximilianoaguero@aol.com>
2025-08-05 17:58:42 -0300

    Primer commit del proyecto

---
+++ src/main/java/com/example/sq_ch7_ex1/SqCh7Ex1Application.java
@@ -0,0 +1,15 @@
+package com.example.sq_ch7_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh7Ex1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SqCh7Ex1Application.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/example/sq_ch7_ex1/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.example.sq_ch7_ex1.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping({"/", "/home"})
+    public String home(Model page) {
+
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", "red");
+
+        return "home.html";
+    }
+}
